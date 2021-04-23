43f244f877e36da215a6a916abf4f312dcdd2ecc
art-bug <artbag94@gmail.com>
2021-04-23 17:35:27 +0300

    Publishing on Heroku

--- app/WarehouseWebAPI/WarehouseWebAPI/settings.py
+++ app/WarehouseWebAPI/WarehouseWebAPI/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -130,4 +131,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
